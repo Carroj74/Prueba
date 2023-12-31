e06c218d542e44045af6e83d25dacb2b321cd976
Carroj74 <126189209+Carroj74@users.noreply.github.com>
2023-12-30 23:05:10 -0500

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,13 +1,13 @@
 #programa para resolver el problema foobar
-numero=input("Digite el número ")
+numero=input("Digite el número ") #Se solicita la digitación del número a comprobar y se obtiene la cantidad de digitos del numero ingresado
 ultima=int(numero)%10
 suma=0
 
-tam=len(numero)
-for i in range (tam):
+tam=len(numero) #Se calcula la longitud del numero y se emplea el bucle for para efectuar la sumatoria 
+for i in range (tam): 
  suma=suma+int(numero[i])
  
-div3=suma%3
+div3=suma%3 #Se realiza validación con las condicionales correspondientes para realizar el proceso
 div5=ultima%5
 if div3==0 and ultima==0 or ultima==5:
     print("Foobar")
