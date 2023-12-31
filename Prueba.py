b226ce4a2bba6ecd3dc4fe0537397d920c128ad8
Carroj74 <126189209+Carroj74@users.noreply.github.com>
2023-12-30 22:49:59 -0500

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,4 +1,4 @@
-//programa para resolver el problema foobar//
+#programa para resolver el problema foobar
 numero=input("Digite el número ")
 ultima=int(numero)%10
 suma=0
@@ -8,16 +8,16 @@ for i in range (tam):
  suma=suma+int(numero[i])
  
 div3=suma%3
-if div3==0:
-    print ("Foo")
-
 div5=ultima%5
-if ultima==5 or ultima==0:
+if div3==0 and ultima==0 or ultima==5:
+    print("Foobar")
+else:
+ if div3==0:
+    print ("Foo")
+ else:
+  if ultima==5 or ultima==0:
     print("Bar")
 
-if div3==0 or ultima==5 or ultima==0:
-    print("Foobar")
-  
 if div3!=0 and ultima!=5 and ultima!=0:
     print(numero)
 
